6326c61e52303fe49c0f11af2bcf3d60a764a605
gantay11 <sanskargurung412@gmail.com>
2026-03-10 20:35:39 +0600

    titok

--- src/app/page.js
+++
@@ -1,9 +0,0 @@
-import VideoFeed from' ../components/ui/Videofeed';
-
-export default function Home() {
-  return (
-    <div className="py-4">
-      <VideoFeed />
-    </div>
-  );
-}
--- titok/src/app/page.js
+++ titok/src/app/page.js
@@ -1,8 +1,9 @@
+import VideoFeed from' ../components/ui/Videofeed';
+
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 className="text-4xl font-bold">TikTok Clone!</h1>
-      <p className="mt-4 text-xl">Welcome to our TikTok project!</p>
-    </main>
+    <div className="py-4">
+      <VideoFeed />
+    </div>
   );
 }
